13cf5021e8d7ef331ee0ca381597f68d3c739632
Jerry Chen <jerryq.chen@gmail.com>
2025-07-29 20:28:22 +0800

    added basic folder

---
+++ lessons/lesson25.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# program to output a system page
+
+echo "<html>
+         <head>
+            <title>System Page</title>
+        </head>
+           <body>
+                page body
+            </body>
+        </html>"
