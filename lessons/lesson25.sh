445807a4d01de4bce63cbdb08f95b11d03483e08
Jerry Chen <jerryq.chen@gmail.com>
2025-08-01 21:22:02 +0800

    lesson 25 26

--- lessons/lesson25.sh
+++ lessons/lesson25.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # program to output a system page
 
-echo "<html>
+TITLE="System information report for $HOSTNAME"
+CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+USER=$(whoami)
+TIME_STAMP="Generated on $CURRENT_TIME, by $USER"
+
+# upper case for constants
+# lower case for variables
+cat << _EOF_
+<html>
          <head>
-            <title>System Page</title>
+            <title>$TITLE</title>
         </head>
            <body>
-                page body
+                <h1>$TITLE</h1>
+                <p>$TIME_STAMP</p>
             </body>
         </html>"
+_EOF_
---
+++ lessons/lesson26.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shell function
+
+function step2(){
+    echo "Step 2"
+    return 
+}
+
+echo "Step 1"
+step2
+echo "Step 3"
